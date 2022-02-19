080718e011f73f923ca6cc2b2fc6561b6ee0cfe2
datttsu <97794455+datttsu@users.noreply.github.com>
2022-02-19 21:19:59 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,57 @@
+$(".openbtn").click(function () {//ボタンがクリックされたら
+	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+    $("#container").toggleClass('mainblur');//ぼかしたいエリアにmainblurクラスを付与
+});
+
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスを除去し
+    $("#container").removeClass('mainblur');//ぼかしているエリアのmainblurクラスを除去
+});
+
+
+ //wowjs
+ new WOW().init()
+ //こうすることでwowが正常に機能する
+
+
+
+//スクロールした際の動きを関数でまとめる
+function PageTopAnime() {
+	var scroll = $(window).scrollTop();
+	if (scroll >= 100){//上から100pxスクロールしたら
+		$('#page-top').removeClass('DownMove');//#page-topについているDownMoveというクラス名を除く
+		$('#page-top').addClass('UpMove');//#page-topについているUpMoveというクラス名を付与
+	}else{
+		if($('#page-top').hasClass('UpMove')){//すでに#page-topにUpMoveというクラス名がついていたら
+			$('#page-top').removeClass('UpMove');//UpMoveというクラス名を除き
+			$('#page-top').addClass('DownMove');//DownMoveというクラス名を#page-topに付与
+		}
+	}
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+	PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+	PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+
+// #page-topをクリックした際の設定
+$('#page-top').click(function () {
+	var scroll = $(window).scrollTop(); //スクロール値を取得
+	if(scroll > 0){
+		$(this).addClass('floatAnime');//クリックしたらfloatAnimeというクラス名が付与
+        $('body,html').animate({
+            scrollTop: 0
+        }, 2000,function(){//スクロールの速さ。数字が大きくなるほど遅くなる
+            $('#page-top').removeClass('floatAnime');//上までスクロールしたらfloatAnimeというクラス名を除く
+        });	
+	}
+    return false;//リンク自体の無効化
+});
